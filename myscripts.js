c6cc53a9a19b8119649d8f8542f85119418549b4
oluschi <72060680+oluschi@users.noreply.github.com>
2021-03-01 19:50:05 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.add("mystyle");
+}
+
+
+
+
+
+
+function openForm() {
+  document.getElementById("myDIV").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myDIV").style.display = "none";
+}
